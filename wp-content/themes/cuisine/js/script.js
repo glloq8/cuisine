4033aa522b9f94d0f8a74e8c864757b105a9e4a5
Yannick <ratelyannick@gmail.com>
2022-08-04 13:50:42 +0200

    update gestion header avec cookie

--- wp-content/themes/cuisine/js/script.js
+++ wp-content/themes/cuisine/js/script.js
@@ -2,12 +2,15 @@ let downArrow = document.querySelector('.down');
 
 downArrow.addEventListener('click', function() {
     document.querySelector('.header').classList.toggle('active');
+    document.querySelector('body').classList.remove('first-visite');
 
     document.cookie = 'header=active';
 });
 
 if( document.cookie.indexOf('header=active') > -1 ) {
     document.querySelector('.header').classList.add('hidden');
+} else {
+    document.querySelector('body').classList.add('first-visite');
 }
 
 let ingredientsList = document.querySelectorAll('.ingredients li');
